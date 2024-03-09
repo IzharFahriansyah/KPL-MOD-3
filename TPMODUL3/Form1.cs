e01d6ec9568a74198cf9cfcb5776c379b076055c
unknown <izharfahriansyah7@gmail.com>
2024-03-09 08:16:41 +0700

    Third Commit

--- TPMODUL3/Form1.cs
+++ TPMODUL3/Form1.cs
@@ -6,5 +6,25 @@ namespace TPMODUL3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
